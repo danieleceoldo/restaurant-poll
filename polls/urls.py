57e2118543c14772d14abf55c72a38db7e7462e1
Daniele Ceoldo <daniele.ceoldo@gmail.com>
2017-02-17 17:55:30 +0100

    GPLv3 disclaimer added in python code

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,27 @@
+#
+# project: restaurant-poll
+# author: Daniele Ceoldo
+# 
+#
+#
+# This file is part of restaurant-poll.
+#
+# restaurant-poll is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# restauran-poll is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with restaurant-poll.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+
 from django.conf.urls import url
 from . import views
 
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+#
+# project: restaurant-poll
+# author: Daniele Ceoldo
+# 
+#
+#
+# This file is part of restaurant-poll.
+#
+# restaurant-poll is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# restauran-poll is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with restaurant-poll.  If not, see <http://www.gnu.org/licenses/>.
+#
 
-# Create your views here.
 
-from django.shortcuts import get_object_or_404
+
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponseRedirect
 from django.urls import reverse
